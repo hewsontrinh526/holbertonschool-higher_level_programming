5d5ceef0f026d5628bb4718066b3d11f26f4d0af
hewsontrinh526 <6734@holbertonstudents.com>
2023-09-25 12:59:43 +1000

    Feat: A program that capitilises all the letter in a string

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    upper = ""
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            i = chr(ord(i) - 32)
-        print("{}".format(i), end="")
-    print("")
+            upper = upper + chr(ord(i) - 32)
+        else:
+            upper = upper + i
+    print("{}".format(upper))    
    
