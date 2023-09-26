cbe01b451ce20036cde344e889461f42adab384a
hewsontrinh526 <6734@holbertonstudents.com>
2023-09-26 11:01:41 +1000

    Feat: A calculator that support all functions from the file calculator-1

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,17 +5,21 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
+
     if operator == "+":
-        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+        result = add(a, b)
     elif operator == "-":
-        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+        result = sub(a, b)
     elif operator == "*":
-        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+        result = mul(a, b)
     elif operator == "/":
-        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+        result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
