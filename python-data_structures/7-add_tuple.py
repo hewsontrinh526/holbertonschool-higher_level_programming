bab847c37e557f59a89cb2a59c690a869cfc4cba
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-02 14:47:54 +1100

    Feat: A function that adds 2 tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+
+    if len(list_a) < 2:
+        list_a = list_a + [0] * (2 - len(list_a))
+    if len(list_b) < 2:
+        list_b = list_b + [0] * (2 - len(list_b))
+
+    new_a = list_a[0] + list_b[0]
+    new_b = list_a[1] + list_b[1]
+    
+    new_list = (new_a, new_b)
+    new_tuple = tuple(new_list)
+
+    return (new_tuple)
