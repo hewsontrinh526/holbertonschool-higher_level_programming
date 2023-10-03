03a986caf323c7523865af79f77f37257eac206a
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-03 15:28:10 +1100

    Feat: A function that converts a Roman numerals

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
-        return 0
-
     rom_dict = {
         "M": 1000, "D": 500, "C": 100, "X": 10, "L": 50, "V": 5, "I": 1
         }
+
+    if roman_string is None or type(roman_string) is not str:
+        return 0
 
     num = 0
 
