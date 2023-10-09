cc7aa57bece011d803b3b384be96bf35252ce66d
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-09 20:34:28 +1100

    Feat: A class square that defines a square by a private instance attribute of size with conditions

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,12 +3,27 @@
 
 
 class Square:
-    """Contains a private instance attribute of size with conditions"""
+    """Contains a private instance attribute of size with conditions
+    
+    Attributes:
+        size (int): size of the square
+    Methods:
+        __init__(self, size=0): Initialises a new instance of a square
+    """
     def __init__(self, size=0):
-        if type(size) == int:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
-        else:
+        """
+        Initialises a new square instance
+
+        Arguments:
+        size (int): The size of the square, set to 0 by default
+
+        Raises:
+        TypeError: If size is not of type integer
+        ValueError: If size is not greater than 0
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
