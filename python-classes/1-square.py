88ebb2e59cfb6ac2820ba5b2a65a596db4acd573
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-09 20:09:26 +1100

    Feat: A class Square that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Contains a private instance attribute of size"""
+    def __init__(self, size):
+        self.__size = size
