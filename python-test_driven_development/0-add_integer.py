fb3e5d01602023bd463e29d38c8a3f1c7a34d8dc
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-11 12:08:41 +1100

    Fix: Adding documentation to function file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+"""
+Module: 0-add_integer.py
+
+Function: add_integer()
+"""
+
+
 def add_integer(a, b=98):
+    """
+    add_integer: Adds integers/floats together
+
+    Args:
+    a and b are inputs, b defaults to 98
+
+    Returns:
+    The sum of a and b as an int. a and b are converted before addition
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
