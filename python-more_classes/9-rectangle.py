6d51e3a19b7093651bce1569c7d9d0014db6f21c
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-12 17:05:25 +1100

    Feat: Added function to use, allows for printing a square using Rectangle class

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -136,6 +136,7 @@ class Rectangle:
         Rectangle.number_of_instances = Rectangle.number_of_instances - 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
         Compares two rectangles and determines whether one rectangle is
