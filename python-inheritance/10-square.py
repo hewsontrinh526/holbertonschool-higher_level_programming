3bf6fcf4a41b0146cea038c90abb7b25738dff4b
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-17 07:13:57 +1100

    Feat: Added subclass Square that inherits from Rectangle, privatised size and implemented area()

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module: 10-square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A subclass Square that inherits from Rectangle
+    """
+    def __init__(self, size):
+        """
+        Size is privatised
+        """
+        self.__size = size
+        super().integer_validator("size", self.__size)
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Finds the area of the square
+        """
+        return super().area()
