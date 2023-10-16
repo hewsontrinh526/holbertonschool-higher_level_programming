26e6367b85a687af14d765fcfa98016b20534c7e
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-16 18:13:55 +1100

    Feat: Expanded on 5-base_geometry, added public instance method: def area(self)

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module: 6-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    Public instance method: def area(self):
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
