3107421fe6d5487f62ac84d9e9eeab317910c50b
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-16 13:02:57 +1100

    Feat: A function that returns the list of avaliable attributes and methods of an object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+Module: A function that returns a list of avaliable
+attributes and methods of an object
+"""
+
+
 def lookup(obj):
+    """
+    Returns a list of avaliable attributes and methods
+    of an object
+    """
     return dir(obj)
