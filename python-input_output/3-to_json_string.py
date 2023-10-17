4fd25be96ced1a78061fa935f2bb6f81af52b108
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-17 21:10:55 +1100

    Feat: A function that returns the JSON representation of an object (string)

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -10,4 +10,4 @@ def to_json_string(my_obj):
     A function that returns the JSON
     representation of an object (string)
     """
-    return print(json.dumps(my_obj))
+    return json.dumps(my_obj)
