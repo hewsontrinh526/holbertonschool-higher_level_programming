12c6beafcb20bab494e1d2362f5c181f24708dc7
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-19 17:47:43 +1100

    Feat: A function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialisation of an object

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module: 8-class_to_json
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    A function that returns the dictionary description with simple data
+    structure (list, dictionary, string, integer and boolean) for JSON
+    serialisation of an object.
+    """
+    return obj.__dict__
