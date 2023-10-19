94f50a924484c3dd621a9d9d499f359e24338db2
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-19 19:26:15 +1100

    Feat: A function that returns a list of lists of integers representing the Pascals triangle of n

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module: 12-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    A function that will return a list of lists integers representing the
+    Pascal's triangle of n.
+    """
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
