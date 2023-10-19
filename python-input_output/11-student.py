f0eb0833f3f42615a2ebda3c56894801b2809784
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-19 18:49:47 +1100

    Feat: Adding a public method def reload_from_json(self, json) that replaces all attributes of the Student instance

---
+++ python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module: 10-student
+"""
+
+
+class Student:
+    """
+    A class Student that defines a student by first_name, last_name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        A method that returns the dictionary description with simple data
+        structure (list, dictionary, string, integer and boolean) for JSON
+        serialisation of an object.
+        """
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for i in attrs:
+            if hasattr(self, i):
+                new_dict[i] = getattr(self, i)
+        return new_dict
+
+    def reload_from_json(self, json):
+        """
+        A method that replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
