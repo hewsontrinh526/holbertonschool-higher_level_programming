13c4a99413507fcc0153a5407b1074071ad1356e
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-26 11:54:09 +1100

    Feat: Added unittest for basy.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -22,3 +22,52 @@ class TestBase(unittest.TestCase):
         self.assertNotEqual(base_1.id, base_2.id)
         base_3 = Base(45)
         self.assertEqual(base_3.id, 45)
+
+    def test_to_json_string(self):
+        """
+        Testing for returning JSON string representation
+        """
+        """
+        Test with an empty list
+        """
+        base_data_1 = []
+        json_string_1 = Base.to_json_string(base_data_1)
+        self.assertEqual(json_string_1, "[]")
+        """
+        Test for correct output of JSON string representation
+        """
+        base_data_2 = [{"Mina": "1"}, {"Nayeon": "2"}, {"Jihyo": "3"}]
+        json_string_2 = Base.to_json_string(base_data_2)
+        self.assertEqual(json_string_2
+                         , '[{"Mina": "1"}, {"Nayeon": "2"}, {"Jihyo": "3"}]')
+        """
+        Test for correct output of JSON string representation if the input
+        is 'None'
+        """
+        base_data_3 = None
+        json_string_3 = Base.to_json_string(base_data_3)
+        self.assertEqual(json_string_3, '[]')
+
+    def test_from_json_string(self):
+        """
+        Testing for inputing JSON string representation
+        """
+        """
+        Test for string input
+        """
+        json_string_1 = '[{"Mina": "1"}, {"Nayeon": "2"}, {"Jihyo": "3"}]'
+        base_data_1 = Base.from_json_string(json_string_1)
+        self.assertEqual(base_data_1
+                        , [{"Mina": "1"}, {"Nayeon": "2"}, {"Jihyo": "3"}])
+        """
+        Test for empty string input
+        """
+        json_string_2 = ''
+        base_data_2 = Base.from_json_string(json_string_2)
+        self.assertEqual(base_data_2, [])
+        """
+        Test for None string input
+        """
+        json_string_3 = None
+        base_data_3 = Base.from_json_string(json_string_3)
+        self.assertEqual(base_data_3, [])
