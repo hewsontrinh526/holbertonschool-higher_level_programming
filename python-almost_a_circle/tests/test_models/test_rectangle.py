b18ee48967b625034b2a52dda5932a2f31e85d77
hewsontrinh526 <6734@holbertonstudents.com>
2023-10-27 17:32:54 +1100

    Feat: Added unittest for test_rectangle.py

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -15,6 +15,14 @@ class TestRectangle(unittest.TestCase):
     """
     Tests for Rectangle Class
     """
+    def test_rectangle_only(self):
+        """
+        Test for correct input
+        """
+        rectangle_1 = Rectangle(1, 2)
+        self.assertEqual(rectangle_1.width, 1)
+        self.assertEqual(rectangle_1.height, 2)
+
     def test_rectangle_correct_arguments(self):
         """
         Test for correct input
