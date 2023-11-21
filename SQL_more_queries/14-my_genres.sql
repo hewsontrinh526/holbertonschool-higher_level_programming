9592f723cd304f43b7089f792971096dca7d098a
hewsontrinh526 <6734@holbertonstudents.com>
2023-11-21 18:30:15 +1100

    Feat: Finds all the genres linked to the show Dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,6 +2,6 @@
 SELECT tv_genres.name
 FROM tv_show_genres
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN v_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
