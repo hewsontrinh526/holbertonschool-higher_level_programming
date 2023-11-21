b885ae1530a370dbf61a041af0e390943a6fec1a
hewsontrinh526 <6734@holbertonstudents.com>
2023-11-21 17:07:27 +1100

    Feat: Lists all cities contained in the database hbtn_0d_usa

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- Lists all cities contained in the database hbtn_0d_usa
-SELECT states.id, states.name, states.state_id FROM cities
+SELECT id, name, state_id
+FROM cities
 LEFT JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
