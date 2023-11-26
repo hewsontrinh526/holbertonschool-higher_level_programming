e9484a2d9de49d9f885814dd9176542b7dc8285c
hewsontrinh526 <6734@holbertonstudents.com>
2023-11-26 16:07:19 +1100

    Feat: Added ordering by id

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     database = MySQLdb.connect(host="localhost", user=sys.argv[1],
                                passwd=sys.argv[2], db=sys.argv[3])
     cursor = database.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
