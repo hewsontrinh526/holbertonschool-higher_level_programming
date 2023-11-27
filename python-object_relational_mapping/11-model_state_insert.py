ee7183881b44061d8d8f72657cc65e6c32d3c395
hewsontrinh526 <6734@holbertonstudents.com>
2023-11-27 20:37:02 +1100

    Feat: Script that adds the State object Louisiana to the database hbtn_0e_6_usa

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
 """
-Scripts that adds the State object "Louisiana" to the database hbtn_0e_6_usa
+Script that prints the State object with the name passed as argument from the
+database hbtn_0e_6_usa
 """
 import MySQLdb
 import sys
 from sqlalchemy import create_engine
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     database = MySQLdb.connect(host="localhost", user=sys.argv[1],
                                passwd=sys.argv[2], db=sys.argv[3])
     cursor = database.cursor()
-    cursor.execute("INSERT INTO states (name) VALUES ('Louisiana');")
-    print("{}".format(cursor.lastrowid))
+    cursor.execute("SELECT id FROM states WHERE name LIKE %s ORDER BY id;",
+                   ('Louisiana',))
+    rows = cursor.fetchall()
+    if not rows:
+        new_state = State(name="Louisiana")
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                               .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        session.add(new_state)
+        session.commit()
+        print(new_state.id)
+        session.close()
+    else:
+        for row in rows:
+            print("{}".format(row[0]))
     cursor.close()
     database.close()
