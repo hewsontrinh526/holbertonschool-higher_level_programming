fafe05347dc5d21f56b3205900f8e991d0d7908c
hewsontrinh526 <6734@holbertonstudents.com>
2023-11-27 20:49:40 +1100

    Fix: pycodestyle

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     new_state = State(name="Louisiana")
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
     state_update = session.query(State).filter(State.id == 2).first()
