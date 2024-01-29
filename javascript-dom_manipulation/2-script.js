d0d542cf1915b6e06a83b59301c4db7acdfab8af
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-29 17:35:56 +1100

    Feat: A JavaScript script that addes the class red to the header element when the user clicks on the tag with the id red_header

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector("#red_header");
+
+header.onclick = colorChange;
+
+function colorChange() {
+    let headerElement = document.getElementsByTagName("header")[0];
+    headerElement.classList.add("red");
+}
