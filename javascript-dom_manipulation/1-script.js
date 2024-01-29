132fbbfb1c2c9e9a80888eef73996cb45b2482e9
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-29 17:25:51 +1100

    Feat: A JavaScript script that updates the text color of the header element to red when the user clicks on the tag with id red_header

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+const redHeader = document.querySelector("#red_header");
+
+redHeader.onclick = colorChange;
+
+function colorChange() {
+    let headerElement = document.getElementsByTagName("header")[0];
+    headerElement.style.color = "#FF0000";
+}
