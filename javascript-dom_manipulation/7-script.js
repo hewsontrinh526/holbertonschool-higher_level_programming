5b69c93389e90420e45e3b75e89f156c86ce42ee
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-30 07:27:24 +1100

    Feat: A JavaScript script that fetches and lists the title for all movies by using the provided URL

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,16 @@
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+const listMovies = document.querySelector('#list_movies');
+
+async function fetchMovies() {
+    const response = await fetch(url);
+    const data = await response.json();
+    const movies = data.results;
+
+    for (const movie of movies) {
+        const newMovie = document.createElement('li');
+        newMovie.textContent = movie.title;
+        listMovies.appendChild(newMovie);
+    }
+}
+
+fetchMovies();
