525df1a28ad5c019e161dd0eba351fc839143628
hewsontrinh526 <6734@holbertonstudents.com>
2024-01-30 07:28:00 +1100

    Feat: A JavaScript script that fetches from and dsiplays the value of hellow from that fetch in the HTML element with the id hello

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    async function fetchHello() {
+        const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+        const selectHello = document.getElementById("hello");
+        const response = await fetch(url);
+        const data = await response.json();
+
+        selectHello.textContent = data.hello;
+    }
+
+    fetchHello();
+
+});
